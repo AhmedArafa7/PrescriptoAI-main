e81b3bb82d3f227579a982d819edcccfb92bbf31
AhmedArafa7 <mo1999382@gmail.com>
2025-06-13 21:38:56 +0300

    v6

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../../core/services/theme/theme.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'] // خليه style**Urls** هنا
 })
